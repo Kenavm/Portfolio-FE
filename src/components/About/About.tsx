539f37f6ee05b13bda6277dba2fab4d19cb10000
Manuel <manuel.vanek@gmail.com>
2023-11-07 18:44:10 +0100

    add header and about

---
+++ src/components/About/About.tsx
@@ -0,0 +1,15 @@
+interface AboutMeProps {
+    image: string,
+    header: string,
+    text:string
+}
+
+const About: React.FC<AboutMeProps> = ({image, header, text}) => {
+    return <div className="about-container">
+        <img src={image} className="image"></img>
+        <h2 className="header">{header}</h2>
+        <p className="text">{text}</p>
+    </div>;
+}
+
+export default About;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return <div className="header">
+    <>HOME LOGOUT</>
+  </div>;
+};
+
+export default Header;
