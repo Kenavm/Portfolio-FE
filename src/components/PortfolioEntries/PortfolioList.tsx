4087afa282ecc6c57e95ad1ced87dc2ad6db6d0f
Theresa Grimus <T.Grimus@gmx.net>
2023-11-09 20:46:25 +0100

    Technology file to types, AddEntry components, changed ProjectPage, not finished yet

--- src/components/PortfolioEntries/PortfolioList.tsx
+++ src/components/PortfolioEntries/PortfolioList.tsx
@@ -1,20 +1,17 @@
 import Entry from "../../types/Entry";
-import Button from "../Button/Button";
 import PortfolioEntry from "../PortfolioEntry/PortfolioEntry";
 
 
 interface PortfolioEntriesProps {
   entries: Array<Entry>;
-  onclickAddEntry : () => void;
 }
 
-const PortfolioEntries: React.FC<PortfolioEntriesProps> = ({ entries, onclickAddEntry }) => {
+const PortfolioEntries: React.FC<PortfolioEntriesProps> = ({ entries }) => {
   return (
     <div className="portfolio-entries">
       {entries.map((entry) => (
         <PortfolioEntry key={entry.id} entry={entry} />
       ))}
-      <Button onclick={onclickAddEntry} buttonText={"Add entry"}/>
     </div>
   );
 };
--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -2,14 +2,21 @@ import { useEffect, useState } from "react";
 import PortfolioList from "../components/PortfolioEntries/PortfolioList";
 import fetchPortfolioEntries from "../api/fetchPortfolioEntries";
 import fetchSkillEntries from "../api/fetchSkillEntries";
-import Entry from '../types/Entry'
-import Skill from '../types/Skill'
+import Entry from "../types/Entry";
+import Skill from "../types/Skill";
 import SkillList from "../components/SkillList/SkillList";
-import Header from '../components/Header/Header'
+import Header from "../components/Header/Header";
+import AddEntry from "../components/AddEntry/AddEntry";
+import Technology from "../types/Technology";
 
 const ProjectPage = () => {
   const [entries, setEntries] = useState<Array<Entry>>([]);
   const [skills, setSkills] = useState<Array<Skill>>([]);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [roleValue, setRoleValue] = useState("");
+  const [repoLink, setRepoLink] = useState("");
+  const [technologies, setTechnologies] = useState([]);
 
   useEffect(() => {
     const loadSkills = async () => {
@@ -27,15 +34,46 @@ const ProjectPage = () => {
     loadPortfolioEntries();
   }, []);
 
-  const onclickAddEntry = () => {
+  useEffect(() => {
+    const loadTechnologies = () => {
+      const technologies = Object.keys(Technology).map((key, index) => ({
+        id: index + 1,
+        technology: Technology[key as keyof typeof Technology],
+        isChecked: false,
+      }));
+      setTechnologies(technologies);
+    };
+
+    loadTechnologies();
+  }, []);
+
+  const onSubmitEntry = () => {
     console.log("test");
-  }
+  };
+
+  const onCheckedTechnology = (id: number) => {
+    console.log(id);
+  
+  };
 
   return (
     <div>
       <Header />
-      <PortfolioList entries={entries} onclickAddEntry={onclickAddEntry}/>
+      <PortfolioList entries={entries} />
       <SkillList skills={skills} />
+      <AddEntry
+        onSubmitEntry={onSubmitEntry}
+        startDate={startDate}
+        onChangeStartDate={(date) => setStartDate(date)}
+        endDate={endDate}
+        onChangeEndDate={(date) => setEndDate(date)}
+        roleValue={roleValue}
+        onChangeRole={(event) => setRoleValue(event.target.value)}
+        repoLink={repoLink}
+        onChangeLink={(event) => setRepoLink(event.target.value)}
+        technologiesArray={technologies}
+        onChecked={onCheckedTechnology}
+      />
     </div>
   );
 };
