8ad34c7679f2e852062ec828e5995e5b878909b1
Theresa Grimus <T.Grimus@gmx.net>
2023-11-30 18:15:22 +0100

    added cursor pointer to home div

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import './Header.css'
+import "./Header.css";
 
 const Header = () => {
   const navigate = useNavigate();
 
-  return <div className="w-full header">
-    <img className="h-28 flex justify-start items-end" src='/Buildfolio.jpg'></img>
-    <div className="flex justify-end items-start" onClick = {() => navigate("/") }>
+  return (
+    <div className="w-full header">
+      <img
+        className="h-28 flex justify-start items-end"
+        src="/Buildfolio.jpg"
+      ></img>
+      <div
+        className="flex justify-end items-start cursor-pointer ..."
+        onClick={() => navigate("/")}
+      >
         <>HOME</>
+      </div>
     </div>
-  
-  </div>;
+  );
 };
 
 export default Header;
