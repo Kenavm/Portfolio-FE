ea351056058ab6f59a10a392c79db508d8ed2e85
Theresa Grimus <T.Grimus@gmx.net>
2023-11-30 16:47:49 +0100

    added home button functinality in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import './Header.css'
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return <div className="w-full header">
     <img className="h-28 flex justify-start items-end" src='/Buildfolio.jpg'></img>
-    <div className="flex justify-end items-start">
-        <>HOME LOGOUT</>
+    <div className="flex justify-end items-start" onClick = {() => navigate("/") }>
+        <>HOME</>
     </div>
   
   </div>;
