395436b05b51742da2119d9aaa0064cc75819882
Theresa Grimus <T.Grimus@gmx.net>
2023-12-01 16:27:46 +0100

    changed PublicPage file

--- src/components/PublicPage/PublicPage.tsx
+++ src/components/PublicPage/PublicPage.tsx
@@ -1,60 +1,61 @@
 import { useEffect, useState } from "react";
 import PageDTO from "../../types/PageDTO";
-import Skill from "../../types/Skill";
 import { useParams } from "react-router-dom";
-import fetchSkillEntries from "../../api/fetchSkillEntries";
 import React from "react";
 import Header from "../Header/Header";
 import About from "../About/About";
 import PortfolioList from "../PortfolioEntries/PortfolioList";
 import SkillList from "../SkillList/SkillList";
 import fetchPageDTO from "../../api/fetchPageDTO";
+import fetchAllTechnologies from "../../api/fetchAllTechnologies";
 
+const PublicPage = () => {
+  const [pageDTO, setPageDTO] = useState<PageDTO>();
+  const [technologies, setAllTechnologies] = useState([]);
+  const { userId } = useParams();
 
-const PublicPage = ({}) => {
-    const [skills, setSkills] = useState<Array<Skill>>([]);
-    const [pageDTO, setPageDTO] = useState<PageDTO>();
-    const { userId } = useParams();
-  
-    useEffect(() => {
-      const loadSkills = async () => {
-          const skills = await fetchSkillEntries();
-          setSkills(skills);
-      };
-  
-      loadSkills();
-      loadPageDTO();
-    
-    }, []);
+  useEffect(() => {
+    loadAllTechnologies();
+    loadPageDTO();
+  }, []);
 
-    const loadPageDTO = async () => {
-      if (userId !== undefined) {
-        const user = await fetchPageDTO(parseInt(userId));
-        setPageDTO(user);
-      }
-    };
-  
-    return (
+  const loadPageDTO = async () => {
+    if (userId !== undefined) {
+      const user = await fetchPageDTO(parseInt(userId));
+      setPageDTO(user);
+    }
+  };
+
+  const loadAllTechnologies = async () => {
+    const allTechnologies = await fetchAllTechnologies();
+    setAllTechnologies(allTechnologies);
+  }
+
+  return (
+    <div>
+      <Header />
       <div>
-        <Header />
-        <div>
-          <div className="flex h-screen">
-            <div className="flex-none">
-              {pageDTO?.publicUser && <About publicUser={pageDTO.publicUser} />}
-            </div>
-            <div className="flex-grow overflow-y-auto">
-              {pageDTO && (
-                <PortfolioList
-                  entries={pageDTO.portfolioEntryList}
-                  loggedIn = {false}
-                />
-              )}
-            </div>
+        <div className="flex h-screen">
+          <div className="flex-none">
+            {pageDTO?.publicUser && <About publicUser={pageDTO.publicUser} />}
           </div>
-          <SkillList skills={skills} />
+          <div className="flex-grow overflow-y-auto">
+            {pageDTO && (
+              <PortfolioList
+                entries={pageDTO.portfolioEntryList}
+                loggedIn={false}
+              />
+            )}
+          </div>
         </div>
+        {pageDTO?.publicUser.skillList !== undefined ? (
+          <SkillList skills={pageDTO?.publicUser.skillList}  technologies= {technologies}/>
+        ) : (
+          <p></p>
+        )}
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default PublicPage;
