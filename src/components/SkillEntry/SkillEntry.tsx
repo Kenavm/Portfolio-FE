20feecc827e59ae8f0b28e0afbf743820f9b233e
Theresa Grimus <T.Grimus@gmx.net>
2023-12-01 18:45:29 +0100

    finished

--- src/components/SkillEntry/SkillEntry.tsx
+++ src/components/SkillEntry/SkillEntry.tsx
@@ -22,7 +22,14 @@ const SkillEntry: React.FC<SkillEntryProps> = ({ skill, technologies }) => {
       <div className="technology">
         <p>{technologyName}</p>
       </div>
-      <div className="level">{String(skill.skillLevel)}</div>
+      <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+        <div
+          className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+          style={{ width: `${skill.skillLevel * 10}%` }}
+        >
+          {" "}
+        </div>
+      </div>
     </div>
   );
 };
